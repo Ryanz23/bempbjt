9fe71bdb873c1efd94ded834d2268ed7f3c3d7a4
Ariyan Andryan Aryja <ariyanandryan@gmail.com>
2024-10-18 23:09:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+showSlides();
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slides img');
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    if (slideIndex < 0) { slideIndex = slides.length - 1; }
+
+    slides.forEach((slide, index) => {
+        slide.style.display = (index === slideIndex) ? 'block' : 'none';
+    });
+}
